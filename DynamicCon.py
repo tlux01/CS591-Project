bd4c0bfd9fd8fe7fc5ec275647b5ba5c359f6bda
Vilin97 <vasilin97@gmail.com>
2020-04-12 20:15:18 -0400

    Wrote the et_cut function. Not sure if it will work because on line 197 join is used in-place. Added a TODO there.
    Also, fixed a typo edge_occurrences. It was edge_occurences before, with one "r".

--- DynamicCon.py
+++ DynamicCon.py
@@ -233,7 +233,7 @@ def et_cut(e, i, dc):
 def et_link(u, v, edge, i, dc):
     # nodes u,v, i is the level, dc is the pointer to the DynamicCon object
 
-    # get active occurence of the nodes
+    # get active occurrence of the nodes
     u_active = dc.G.nodes[u]["data"].active_occ[i]
     v_active = dc.G.nodes[v]["data"].active_occ[i]
 
@@ -245,7 +245,7 @@ def et_link(u, v, edge, i, dc):
     #reroot et_v at v_active
     et_v = change_root(et_v, v_active, i, dc)
 
-    # initialize first 2 of 4 tree occurences corresponding to this edge
+    # initialize first 2 of 4 tree occurrences corresponding to this edge
     dc.G.edges[edge]["data"].tree_occ[i][0] = u_active
     dc.G.edges[edge]["data"].tree_occ[i][1] = new_u_occ
 
@@ -254,16 +254,16 @@ def et_link(u, v, edge, i, dc):
     # v_active = et_v = et_v.first()
     et_v_last = et_v.last()
     dc.G.edges[edge]["data"].tree_occ[i][3] = et_v_last
-    # if they are not the same occurence of the same node
+    # if they are not the same occurrence of the same node
     if et_v_last is not v_active:
         dc.G.edges[edge]["data"].tree_occ[i][2] = v_active
     else:
         dc.G.edges[edge]["data"].tree_occ[i][2] = None
 
-    # update tree occurences of our edge following our the edge after u
-    after_u_edge = u_active.edge_occurences[RIGHT]
+    # update tree occurrences of our edge following our the edge after u
+    after_u_edge = u_active.edge_occurrences[RIGHT]
     if after_u_edge:
-        if u_active.edge_occurences[LEFT] != after_u_edge:
+        if u_active.edge_occurrences[LEFT] != after_u_edge:
             k = 0
             # find pointer to u_active
             while True:
@@ -282,14 +282,14 @@ def et_link(u, v, edge, i, dc):
                     dc.G.edges[after_u_edge]["data"].tree_occ[i][k] = new_u_occ
                     break
 
-    # update edge_occurences
-    new_u_occ.edge_occurences[RIGHT] = u_active.edge_occurences[RIGHT]
-    new_u_occ.edge_occurences[LEFT] = edge
+    # update edge_occurrences
+    new_u_occ.edge_occurrences[RIGHT] = u_active.edge_occurrences[RIGHT]
+    new_u_occ.edge_occurrences[LEFT] = edge
 
-    u_active.edge_occurences[RIGHT] = edge
-    v_active.edge_occurences[LEFT] = edge
+    u_active.edge_occurrences[RIGHT] = edge
+    v_active.edge_occurrences[LEFT] = edge
 
-    et_v_last.edge_occurences[RIGHT] = edge
+    et_v_last.edge_occurrences[RIGHT] = edge
 
 
     et_v = bbt.join(et_v, new_u_occ, dc.et_dummy)
@@ -339,8 +339,8 @@ class DynamicConEdge:
 
         # points to array for each level the 4 node occurrences in EulerTree that
         # represent this edge
-        # at each level the four occurences are ordered in the manner below
-        # [occurence of source of edge, occurence of source of edge, occurence of target of edge, occurence of target of edge]
+        # at each level the four occurrences are ordered in the manner below
+        # [occurrence of source of edge, occurrence of source of edge, occurrence of target of edge, occurrence of target of edge]
         self.tree_occ = None
 
 
@@ -421,7 +421,7 @@ class DynamicCon:
 
         # create some empty lists
         if create_tree_occ:
-            # 4 node occurences in EulerTree
+            # 4 node occurrences in EulerTree
             self.G.edges[edge]["data"].tree_occ = [[None, None, None, None] for _ in range(self.max_level + 1)]
 
         ###IMPLEMENT et_link
