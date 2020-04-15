ae8f8205918446b8a2a43ec56370426b816bcb51
Vilin97 <vasilin97@gmail.com>
2020-04-15 18:15:29 -0400

    Tests work. DC-based alg detects that nodes are connected successfully but makes a mistake if nodes are not connected.

--- Tests.py
+++ Tests.py
@@ -3,46 +3,54 @@ import networkx as nx
 from random import sample
 
 
+def mySample(s):
+    return sample(s,1)[0]
+
 def areConnected(G,node1,node2):
     """test if node1 and node2 are connected in G using bfs. Takes linear time in the number of nodes
     in the connected component of node1"""
-    return node2 in list(nx.bfs_successors(G, node1))
+    return node2 in list(dict(nx.bfs_successors(G, node1)))
 
 def getRandomConnectedNodes(G):
     """return two random connected nodes"""
-    node1 = sample(G.nodes(),1)
-    node2 = sample(list(nx.bfs_successors(G, node1)),1)
+    node1 = mySample(G.nodes())
+    node2 = mySample(list(dict(nx.bfs_successors(G, node1))))
     return node1, node2
 
 def getRandomNotConnectedNodes(G):
     """return two random connected nodes"""
-    node1 = sample(G.nodes(),1)
+    node1 = mySample(G.nodes())
     nodes = set(G.nodes())
-    CC = set(nx.bfs_successors).union(set(node1))
+    CC = set(list(dict(nx.bfs_successors(G,node1))))
     possible_nodes = nodes - CC
-    if possible_nodes == set():
+    if len(possible_nodes) == 0:
         print("Error: G is connected!")
         return None
-    node2 = sample(list(nodes - CC),1)
+    node2 = mySample(list(nodes - CC))
     return node1, node2
 
 def test1(n,p):
     G = nx.gnp_random_graph(n,p)
-    #DC = DynamicCon(G)
+    DC = DynamicCon(G)
     N = 100
     allTrues1 = [False]*N
     allTrues2 = [False]*N
     for i in range(100):
         node1,node2 = getRandomConnectedNodes(G)
         allTrues1[i] = areConnected(G,node1,node2)
-        #allTrues2[i] = DC.connected(node1,node2)
+        allTrues2[i] = DC.connected(node1,node2)
+    print("The BFS-based alg works correctly = " + str(False not in allTrues1))
+    print("The DC-based alg works correctly = " + str(False not in allTrues2))
+
     allFalses1 = [True]*N
+    allFalses2 = [True] * N
     for i in range(100):
         node1,node2 = getRandomNotConnectedNodes(G)
         allFalses1[i] = areConnected(G,node1,node2)
-        #allTrues2[i] = DC.connected(node1,node2)
-    print("The BFS-based alg works correctly = " + str(False not in allTrues1))
+        allFalses2[i] = DC.connected(node1,node2)
+
     print("The BFS-based alg works correctly = " + str(True not in allFalses1))
+    print("The DC-based alg works correctly = " + str(True not in allFalses2))
 
 if __name__ == "__main__":
     test1(200,0.05)
