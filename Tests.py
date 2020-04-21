6ef8df29a0cb162d2804c16ee75e8fbfd8ce2daa
Vilin97 <vasilin97@gmail.com>
2020-04-21 14:03:09 -0400

    Wrote a simple test with adding and deleting edges

--- Tests.py
+++ Tests.py
@@ -9,19 +9,19 @@ def mySample(s):
 def areConnected(G,node1,node2):
     """test if node1 and node2 are connected in G using bfs. Takes linear time in the number of nodes
     in the connected component of node1"""
-    return node2 in list(dict(nx.bfs_successors(G, node1)))
+    return node2 in nx.node_connected_component(G,node1)
 
 def getRandomConnectedNodes(G):
     """return two random connected nodes"""
     node1 = mySample(G.nodes())
-    node2 = mySample(list(dict(nx.bfs_successors(G, node1))))
+    node2 = mySample(nx.node_connected_component(G,node1))
     return node1, node2
 
 def getRandomNotConnectedNodes(G):
     """return two random connected nodes"""
     node1 = mySample(G.nodes())
     nodes = set(G.nodes())
-    CC = set(list(dict(nx.bfs_successors(G,node1))))
+    CC = nx.node_connected_component(G,node1)
     possible_nodes = nodes - CC
     if len(possible_nodes) == 0:
         print("Error: G is connected!")
@@ -39,7 +39,7 @@ def test1(n,p):
     N = 100
     allTrues1 = [False]*N
     allTrues2 = [False]*N
-    for i in range(100):
+    for i in range(N):
         node1,node2 = getRandomConnectedNodes(G)
         allTrues1[i] = areConnected(G,node1,node2)
         allTrues2[i] = DC.connected(node1,node2)
@@ -48,21 +48,38 @@ def test1(n,p):
 
     allFalses1 = [True]*N
     allFalses2 = [True] * N
-    for i in range(100):
+    for i in range(N):
         node1,node2 = getRandomNotConnectedNodes(G)
         allFalses1[i] = areConnected(G,node1,node2)
         allFalses2[i] = DC.connected(node1,node2)
-        if not allFalses2[i]:
+        if allFalses2[i]:
+            print("DC reports that {} and {} are connected!".format(node1, node2))
             print("Node1: {}, node2: {}. CC of node1: {}".format(node1, node2, nx.node_connected_component(G,node1)))
 
     print("The BFS-based alg works correctly = " + str(True not in allFalses1))
     print("The DC-based alg works correctly = " + str(True not in allFalses2))
 
 def test2():
-    """tests DC with adding and removing edges"""
-    # TODO
+    """tests DC with adding edges on n nodes"""
+    G = nx.Graph()
+    for i in range(3):
+        G.add_node(i)
+    DC = DynamicCon(G)
+    DC.ins(0,1)
+    DC.ins(1,2)
+    print("DC works correctly wrt insertion = {}".format(DC.connected(0,1)))
+    print("DC works correctly wrt insertion = {}".format(DC.connected(0, 2)))
+    print("DC works correctly wrt insertion = {}".format(not DC.connected(1, 3)))
+    DC.del_edge(0,1)
+    print("DC works correctly wrt deletion = {}".format(DC.connected(1,2)))
+    print("DC works correctly wrt deletion = {}".format(not DC.connected(0,1)))
+    DC.ins(0, 1)
+    print("DC works correctly wrt insertion = {}".format(DC.connected(0, 1)))
+    print("DC works correctly wrt insertion = {}".format(DC.connected(0, 2)))
+    print("DC works correctly wrt insertion = {}".format(not DC.connected(1, 3)))
 
 if __name__ == "__main__":
     n = 200
-    test1(n, 2/n)
+    # test1(n, 2/n)
+    test2()
     print("Done")
